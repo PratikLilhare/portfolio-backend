db149557a28c7243a6568a486106180268865326
pratik <prateeklilhare@gmail.com>
2022-07-14 13:54:17 +0530

    removed migrations

--- backend/app/migrations/models/1_20220712134930_update.sql
+++
@@ -1,4 +0,0 @@
--- upgrade --
-ALTER TABLE "project" ADD "description" TEXT NOT NULL;
--- downgrade --
-ALTER TABLE "project" DROP COLUMN "description";
--- backend/app/migrations/models/3_20220714044515_update.sql
+++
@@ -1,4 +0,0 @@
--- upgrade --
-ALTER TABLE "project" ALTER COLUMN "languages" DROP DEFAULT;
--- downgrade --
-ALTER TABLE "project" ALTER COLUMN "languages" SET;
